a45e5246e63fb2866f61332915c5902df300d6c7
Mikhail Tyugaev <mikhail.tugayev@hmn.me>
2016-11-23 17:41:56 +0500

    added possibility to separate numbers in simple HTML tags

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 ## Installation
 
-Download jquery.number-separator.js and just add the line
+1. [Download](https://github.com/tyugaev/jquery-number-separator/blob/master/jquery.number-separator.js) jquery.number-separator.js and just add the line
 
-```
-<script type="text/javascript" src="jquery.number-separator.js"></script>
-```
+   ```
+   <script type="text/javascript" src="jquery.number-separator.js"></script>
+   ```
+2. Set up type of inputs to 'text', original value was saved in attr with name 'number'
 
 ## Code Example
 
 With default parameters:
-
 ```javascript
 $('#input').separate();
 ```
 
 Or you can specify parameters:
-
 ```javascript
 $('#input').separate({
     delimiter:' ',
@@ -23,6 +22,18 @@ $('#input').separate({
 });
 ```
 
-## License
+Also you can separate numbers in simple HTML tags
+```html
+<p><div class="separate">1000</div></p>
+<p><span class="separate">10000</span></p>
+<p><label class="separate">1000000</label></p>
+<input class="separate" type="text">
+```
+```javascript
+$('.separate').separate();
+```
+In this case all inputs was bind on change event, but others tags was processed once 
 
+## License
+Copyright &copy; Mikhail Tyugaev<br>
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- jquery.number-separator.js
+++ jquery.number-separator.js
@@ -4,64 +4,94 @@
  * Licensed under the MIT license
  *
  * @author Mikhail Tyugaev
- * @version 0.1.0 (2016-11-15)
-*/
+ * @version 1.0.0 RC (2016-11-23)
+ */
 (function($) {
 
-    var defaults = { 
-        delimiter:',',
-        separateThousand:true, // 1,000..9,999
-        delimiterRegExp:/[\.\,\s]/g
-    };
+	var defaults = {
+		delimiter:',',
+		separateThousand:true, // 1,000..9,999
+		delimiterRegExp:/[\.\,\s]/g
+	};
 
-    $.fn.separate = function(params){
-        options = $.extend({}, defaults, params);
+	$.fn.separate = function(params){
+
+		function isNotInput(elem) {
+			return !elem.is("input, textarea");
+		}
+
+		function isNumeric(num) {
+			return !isNaN(num)
+		}
+
+		function getSeparated(text) {
+			text = text.replace(options.delimiterRegExp, '');
+
+			if (!isNumeric(text) && text.length > 0) {
+				$(this).val('');
+				console.log(text + ' is not a number');
+				return -1;
+			}
+
+			var result = '';
+			var pos = text.length;
+
+			while (pos > 3) {
+				if (text.length == 4) {
+					result = (options.separateThousand ? options.delimiter : '') + text.substring(1);
+					pos = 1;
+					break;
+				}
+				pos -= 3;
+				result = options.delimiter + text.substring(pos, pos + 3) + result;
+			}
+			return text.substring(0, pos) + result;
+		}
+
+		options = $.extend({}, defaults, params);
 
 		this.each(function() {
 
 			var $this = $(this);
 
-        	$this.unbind("input.separate change.separate");
-        });
+			if (isNotInput($this)) {
+				return;
+			}
 
-        this.each(function() {
-			
+			$this.unbind("input.separate change.separate");
+		});
+
+
+		this.each(function() {
+
 			var $this = $(this);
 
-	        $this.bind("input.separate change.separate", function() {
+			if (isNotInput($this)) {
+				var result = getSeparated($this.text());
 
-	            var text = $(this).val().replace(options.delimiterRegExp, '');
+				if (result != -1) {
+					$this.text(result);
+				}
 
-	            if (!isNumeric(text) && text.length > 0) {
-	                $(this).val('');
-	                console.log(text + ' is not a number');
-	                return;
-	            }
+				return;
+			}
 
-	            var result = '';
-	            var pos = text.length;
+			$this.bind("input.separate change.separate", function() {
 
-	            while (pos > 3) {
-	                if (text.length == 4) {
-	                    result = (options.separateThousand ? options.delimiter : '') + text.substring(1);
-	                    pos = 1;
-	                    break;
-	                }
-	                pos -= 3;
-	                result = options.delimiter + text.substring(pos, pos + 3) + result;
-	            }
-	            result = text.substring(0, pos) + result;
+				var text = $(this).val().replace(options.delimiterRegExp, '');
 
-	            $(this).val(result);
-	        });
+				var result = getSeparated(text);
 
-        	$this.trigger('change.separate');
-        });
+				if (result != -1) {
+					$(this).val(result);
+					$(this).attr('number', text);
+				}
+			});
 
-        return this;
-    };
+			$this.trigger("change.separate");
+		});
 
-    function isNumeric(num) {
-        return !isNaN(num)
-    }
+		return this;
+	};
+
 })(jQuery);
